e7644c0a3a9a61b50468d46925c0c5bd8af64c33
zium <imflo7dxepij678t7zz4@gmail.com>
2024-06-03 22:19:10 +0200

    task2 done

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 import sys
+import json
+
+def json_validation():
+    try:
+        with open(sys.argv[1]) as json_file:
+            data = json.load(json_file)
+    except TypeError as Type:
+        print(Type)
+    except FileNotFoundError as File:
+        print(File)
+    except json.decoder.JSONDecodeError as JSON:
+        print(JSON)
+    except Exception as e:
+        print(e)
 
 for x in sys.argv[1:]:
     print(f"Argument {x}")
