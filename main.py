b5134bc58013816f2e1f6d2a1dbdb13ec1aa7ece
zium <imflo7dxepij678t7zz4@gmail.com>
2024-06-03 22:45:54 +0200

    tesk4 done

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 import json
+import yaml
 
 class JsonTask():
     def __init__(self, file):
@@ -27,8 +28,28 @@ class JsonTask():
         except Exception as e:
             print(e)
 
+class YamlTask():
+    def __init__(self, file):
+        self.file = file
+        self.dane = None
+
+    def yaml_validation(self):
+        try:
+            with open(sys.argv[1]) as yaml_file:
+                self.dane = yaml.load(yaml_file, Loader=yaml.FullLoader)
+            print("Dane z pliku YAML zostały poprawnie załadowane i zweryfikowane!")
+        except FileNotFoundError:
+            print("Nie znaleziono takiego pliku")
+        except yaml.scanner.ScannerError:
+            print("Plik yaml jest nieprawidłowy")
+        except Exception as e:
+            print(e)
+
 if __name__ == '__main__':
     if ".json" in sys.argv[1]:
         json_task = JsonTask(sys.argv[1])
         json_task.json_validation()
         json_task.json_save()
+    if ".yaml" in sys.argv[1]:
+        yaml_task = YamlTask(sys.argv[1])
+        yaml_task.yaml_validation()
