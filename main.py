6bbb42c9830c7d4c76f5b5138517adaab85a8d49
zium <imflo7dxepij678t7zz4@gmail.com>
2024-06-03 22:42:52 +0200

    task3 done

--- main.py
+++ main.py
@@ -1,18 +1,34 @@
 import sys
 import json
 
-def json_validation():
-    try:
-        with open(sys.argv[1]) as json_file:
-            data = json.load(json_file)
-    except TypeError as Type:
-        print(Type)
-    except FileNotFoundError as File:
-        print(File)
-    except json.decoder.JSONDecodeError as JSON:
-        print(JSON)
-    except Exception as e:
-        print(e)
+class JsonTask():
+    def __init__(self, file):
+        self.file = file
+        self.dane = None
 
-for x in sys.argv[1:]:
-    print(f"Argument {x}")
+    def json_validation(self):
+        try:
+            with open(sys.argv[1]) as json_file:
+                self.dane = json.load(json_file)
+            print("Dane z pliku JSON zostały poprawnie załadowane i zweryfikowane!")
+        except FileNotFoundError:
+            print("Nie znaleziono takiego pliku")
+        except json.decoder.JSONDecodeError:
+            print("Plik json jest nieprawidłowy")
+        except Exception as e:
+            print(e)
+
+    def json_save(self):
+        try:
+            with open("nowy_plik.json", 'w') as json_file:
+                json.dump(self.dane, json_file)
+        except json.decoder.JSONDecodeError:
+            print("Plik json jest nieprawidłowy")
+        except Exception as e:
+            print(e)
+
+if __name__ == '__main__':
+    if ".json" in sys.argv[1]:
+        json_task = JsonTask(sys.argv[1])
+        json_task.json_validation()
+        json_task.json_save()
